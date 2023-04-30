5f882de8af46d315f02ee0dd2365e12937adabe7
tbnlxex <125676970+tbnlxex@users.noreply.github.com>
2023-04-30 21:26:44 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -1,598 +1,116 @@
 'use strict';
 
-/*********************************************************************************************
- *                                                                                           *
- * Plese read the following tutorial before implementing tasks:                              *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array    *
- *                                                                                           *
- * NOTE : Please do not use loops! All tasks can be implmeneted using standard Array methods *
- *                                                                                           *
- *********************************************************************************************/
+/********************************************************************************************
+ *                                                                                          *
+ * Plese read the following tutorial before implementing tasks:                             *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Numbers_and_dates#Date_object
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date    *
+ *                                                                                          *
+ ********************************************************************************************/
 
- 
+
 /**
- * Returns an index of the specified element in array or -1 if element is not found
- * 
- * @param {array} arr
- * @param {any} value
- * @return {number}
- * 
- * @example
- *    ['Ace', 10, true], 10    => 1 
- *    ['Array', 'Number', 'string'], 'Date'    => -1 
- *    [0, 1, 2, 3, 4, 5], 5    => 5
+ * Parses a rfc2822 string date representation into date value
+ * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
+ *
+ * @param {string} value
+ * @return {date}
+ *
+ * @example:
+ *    'December 17, 1995 03:24:00'    => Date()
+ *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
+ *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function findElement(arr, value) {
-   throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+    return new Date(value);
 }
 
 /**
- * Generates an array of odd numbers of the specified length
- * 
- * @param {number} len
- * @return {array}
- * 
- * @example
- *    1 => [ 1 ] 
- *    2 => [ 1, 3 ] 
- *    5 => [ 1, 3, 5, 7, 9 ]
- */
-function generateOdds(len) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the doubled array - elements of the specified array are repeated twice using original order
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *    ['Ace', 10, true]  => ['Ace', 10, true,   'Ace', 10, true]  
- *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
- *    [] => [] 
- */
-function doubleArray(arr) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Returns an array of positive numbers from the specified array in original order
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *    [ 0, 1, 2, 3, 4, 5 ] => [ 1, 2, 3, 4, 5 ]
- *    [-1, 2, -5, -4, 0] => [ 2 ]
- *    [] => [] 
- */
-function getArrayOfPositives(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Returns the array with strings only in the specified array (in original order)
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *    [ 0, 1, 'cat', 3, true, 'dog' ] => [ 'cat', 'dog' ]
- *    [ 1, 2, 3, 4, 5 ] => []
- *    [ 'cat, 'dog', 'raccon' ] => [ 'cat', 'dog', 'racoon' ]
- */
-function getArrayOfStrings(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Removes falsy values from the specified array
- * Falsy values: false, null, 0, "", undefined, and NaN.
- * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean#Description)
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *    [ 0, false, 'cat', NaN, true, '' ] => [ 'cat', true ]
- *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
- *    [ false, 0, NaN, '', undefined ]   => [ ]
- */
-function removeFalsyValues(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Returns the array of useprcase strings from the specified array
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *    [ 'permanent-internship', 'glutinous-shriek', 'multiplicative-elevation' ] => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ]
- *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
- */
-function getUpperCaseStrings(arr) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the array of string lengths from the specified string array.
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
- *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
- */
-function getStringsLength(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Inserts the item into specified array at specified index
- * 
- * @param {array} arr
- * @param {any} item
- * @param {number} index 
- * 
- * @example
- *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
- *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
- */
-function insertItem(arr, item, index) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Returns the n first items of the specified array
- * 
- * @param {array} arr
- * @param {number} n 
- * 
- * @example
- *    [ 1, 3, 4, 5 ], 2  => [ 1, 2 ]
- *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
- */
-function getHead(arr, n) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the n last items of the specified array
- * 
- * @param {array} arr
- * @param {number} n 
- * 
- * @example
- *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
- *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
- */
-function getTail(arr, n) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Returns CSV represebtation of two-dimentional numeric array.
- * https://en.wikipedia.org/wiki/Comma-separated_values
- * 
- * @param {array} arr
- * @return {string}
- * 
- * @example
- *    [
- *       [  0, 1, 2, 3, 4 ],
- *       [ 10,11,12,13,14 ],
- *       [ 20,21,22,23,24 ],
- *       [ 30,31,32,33,34 ]
- *    ] 
- *           => 
- *     '0,1,2,3,4\n'
- *    +'10,11,12,13,14\n'
- *    +'20,21,22,23,24\n'
- *    +'30,31,32,33,34'
- */
-function toCsvText(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Transforms the numeric array into the according array of squares:
- *   f(x) = x * x
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
- *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
- */
-function toArrayOfSquares(arr) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Transforms the numeric array to the according moving sum array:
- *     f[n] = x[0] + x[1] + x[2] +...+ x[n] 
- *  or f[n] = f[n-1] + x[n]
- * 
- * @param {array} arr
- * @return {array}
- * 
- * Example :
- *   [ 1, 1, 1, 1, 1 ]        => [ 1, 2, 3, 4, 5 ]
- *   [ 10, -10, 10, -10, 10 ] => [ 10, 0, 10, 0, 10 ]
- *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0] 
- *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
- */
-function getMovingSum(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Returns every second item from the specified array:
- * 
- * @param {array} arr
- * @return {array}
- * 
- * Example :
- * [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 2, 4, 6, 8, 10 ]
- * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
- * [ "a" ] => []
- */
-function getSecondItems(arr) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Propagates every item in sequence its position times
- * Returns an array that consists of: one first item, two second items, tree third items etc. 
- * 
- * @param {array} arr 
- * @return {array}
- * 
+ * Parses an ISO 8601 string date representation into date value
+ * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
+ *
+ * @param {string} value
+ * @return {date}
+ *
  * @example :
- *  [] => []
- *  [ 1 ] => [ 1 ]
- *  [ 'a', 'b' ] => [ 'a', 'b','b' ]
- *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
- *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
+ *    '2016-01-19T16:07:37+00:00'    => Date()
+ *    '2016-01-19T08:07:37Z' => Date()
  */
-function propagateItemsByPositionIndex(arr) {
-   throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+    return new Date(value);
 }
 
 
-/** 
- * Returns the 3 largest numbers from the specified array
- * 
- * @param {array} arr
- * @return {array}
+/**
+ * Returns true if specified date is leap year and false otherwise
+ * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
  *
- * @example
- *   [] => []
- *   [ 1, 2 ] => [ 2, 1 ]
- *   [ 1, 2, 3 ] => [ 3, 2, 1 ]
- *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
- *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
+ * @param {date} date
+ * @return {bool}
+ *
+ * @example :
+ *    Date(1900,1,1)    => false
+ *    Date(2000,1,1)    => true
+ *    Date(2001,1,1)    => false
+ *    Date(2012,1,1)    => true
+ *    Date(2015,1,1)    => false
  */
-function get3TopItems(arr) {
-   throw new Error('Not implemented');
-}
- 
- 
-/**  
- * Returns the number of positive numbers from specified array
- * 
- * @param {array} arr
- * @return {number}
- * 
- * @example
- *   [ ]          => 0
- *   [ -1, 0, 1 ] => 1
- *   [ 1, 2, 3]   => 3
- *   [ null, 1, 'elephant' ] => 1
- *   [ 1, '2' ] => 1
- */
-function getPositivesCount(arr) {
-   throw new Error('Not implemented');
-}
- 
-/** 
- * Sorts digit names
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *   [] => []
- *   [ 'nine','one' ]                 => [ 'one', 'nine' ]
- *   [ 'one','two','three' ]          => [ 'one','two', 'three' ]
- *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
- *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
- */
-function sortDigitNamesByNumericOrder(arr) {
-   throw new Error('Not implemented');
+function isLeapYear(date) {
+    if ((date.getFullYear() % 4 == 0 && (date.getFullYear() % 100 != 0)) || date.getFullYear() % 400 == 0)
+        return true;
+    return false;
 }
 
-/** 
- * Returns the sum of all items in the specified array of numbers
- * 
- * @param {array} arr
- * @return {number}
- * 
- * @example
- *   [] => 0
- *   [ 1, 2, 3 ]           => 6
- *   [ -1, 1, -1, 1 ]      => 0
- *   [ 1, 10, 100, 1000 ]  => 1111
- */
-function getItemsSum(arr) {
-   throw new Error('Not implemented');
-}
- 
-/** 
- * Returns the number of all falsy value in the specified array
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *  [] => 0
- *  [ 1, '', 3 ] => 1
- *  [ -1, 'false', null, 0 ] => 2
- *  [ null, undefined, NaN, false, 0, '' ]  => 6
- */
-function getFalsyValuesCount(arr) {
-   throw new Error('Not implemented');
-}
 
 /**
- * Returns a number of all occurences of the specified item in an array  
- * 
- * @param {array} arr
- * @param {any} item 
- * @return {number}
- * 
- * @example
- *    [ 0, 0, 1, 1, 1, 2 ], 1 => 3
- *    [ 1, 2, 3, 4, 5 ], 0 => 0
- *    [ 'a','b','c','c' ], 'c'=> 2
- *    [ null, undefined, null ], null => 2 
- *    [ true, 0, 1, 'true' ], true => 1
- */
-function findAllOccurences(arr, item) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Concatenates all elements from specified array into single string with ',' delimeter  
- * 
- * @param {array} arr 
+ * Returns the string represention of the timespan between two dates.
+ * The format of output string is "HH:mm:ss.sss"
+ *
+ * @param {date} startDate
+ * @param {date} endDate
  * @return {string}
- * 
- * @example
- *    [0, false, 'cat', NaN, true, '']  => '0,false,cat,NaN,true,'
- *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
- *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
- */
-function toStringList(arr) {
-   throw new Error('Not implemented');
-}
-
-
-/**
- * Sorts the specified array by country name first and city name (if countries are equal) in ascending order.
- * 
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *    [
- *      { country: 'Russia',  city: 'Moscow' },
- *      { country: 'Belarus', city: 'Minsk' },
- *      { country: 'Poland',  city: 'Warsaw' },
- *      { country: 'Russia',  city: 'Saint Petersburg' },
- *      { country: 'Poland',  city: 'Krakow' },
- *      { country: 'Belarus', city: 'Brest' }
- *    ]  
- *                      =>
- *    [
- *      { country: 'Belarus', city: 'Brest' },
- *      { country: 'Belarus', city: 'Minsk' },
- *      { country: 'Poland',  city: 'Krakow' },
- *      { country: 'Poland',  city: 'Warsaw' },
- *      { country: 'Russia',  city: 'Moscow' },
- *      { country: 'Russia',  city: 'Saint Petersburg' }
- */
-function sortCitiesArray(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Creates an indentity matrix of the specified size
- * 
- * @param {number} n
- * @return {array}
- * 
- * @example
- *     1  => [[1]]
- *  
- *     2 => [[1,0],
- *           [0,1]]
- * 
- *          [[1,0,0,0,0],
- *           [0,1,0,0,0],
- *     5 =>  [0,0,1,0,0],
- *           [0,0,0,1,0],
- *           [0,0,0,0,1]]   
- */
-function getIdentityMatrix(n) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Creates an array of integers from the specified start to end (inclusive)
- * 
- * @param {number} start
- * @param {number} end
- * @return {array}
- * 
- * @example
- *     1, 5  => [ 1, 2, 3, 4, 5 ]
- *    -2, 2  => [ -2, -1, 0, 1, 2 ]
- *     0, 100 => [ 0, 1, 2, ..., 100 ]
- *     3, 3   => [ 3 ]
- */
-function getIntervalArray(start, end) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Returns array containing only unique values from the specified array.
  *
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *   [ 1, 2, 3, 3, 2, 1 ] => [ 1, 2, 3 ]
- *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
- *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
+ * @example:
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,11,0,0)   => "01:00:00.000"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,30,0)       => "00:30:00.000"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,20)        => "00:00:20.000"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function distinct(arr) {
-   throw new Error('Not implemented');
-}
-
-/**
- * Groups elements of the specified array by key.
- * Returns multimap of keys extracted from array elements via keySelector callback
- * and values extracted via valueSelector callback.
- * See: https://en.wikipedia.org/wiki/Multimap
- *
- * @param {array} array
- * @param {Function} keySelector
- * @param {Function} valueSelector
- * @return {Map}
- *
- * @example
- *   group([
- *      { country: 'Belarus', city: 'Brest' },
- *      { country: 'Russia', city: 'Omsk' },
- *      { country: 'Russia', city: 'Samara' },
- *      { country: 'Belarus', city: 'Grodno' },
- *      { country: 'Belarus', city: 'Minsk' },
- *      { country: 'Poland', city: 'Lodz' }
- *     ], 
- *     item => item.country, 
- *     item => item.city
- *   )
- *            => 
- *   Map {
- *    "Belarus" => ["Brest", "Grodno", "Minsk"],
- *    "Russia" => ["Omsk", "Samara"], 
- *    "Poland" => ["Lodz"]
- *   }
- */
-function group(array, keySelector, valueSelector) {
-   throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+    return (new Date(endDate - startDate)).toISOString().slice(11, -1);
 }
 
 
 /**
- * Projects each element of the specified array to a sequence and flattens the resulting sequences into one array.
+ * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
+ * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
  *
- * @param {array} arr
- * @param {Function} childrenSelector, a transform function to apply to each element that returns an array of children
- * @return {array}
- * 
- * @example
- *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
- *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
- */
-function selectMany(arr, childrenSelector) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Returns an element from the multidimentional array by the specified indexes.
+ * @param {date} date
+ * @return {number}
  *
- * @param {array} arr
- * @param {array} indexes
- * @return {any} element from array
- * 
- * @example
- *   [[1, 2], [3, 4], [5, 6]], [0,0]  => 1        (arr[0][0]) 
- *   ['one','two','three'], [2]       => 'three'  (arr[2]) 
- *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
+ * @example:
+ *    Date.UTC(2016,2,5, 0, 0) => 0
+ *    Date.UTC(2016,3,5, 3, 0) => Math.PI/2
+ *    Date.UTC(2016,3,5,18, 0) => Math.PI
+ *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function getElementByIndexes(arr, indexes) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Swaps the head and tail of the specified array:
- * the head (first half) of array move to the end, the tail (last half) move to the start. 
- * The middle element (if exists) leave on the same position.
- * 
- *  
- * @param {array} arr
- * @return {array}
- * 
- * @example
- *   [ 1, 2, 3, 4, 5 ]   =>  [ 4, 5, 3, 1, 2 ]
- *    \----/   \----/         
- *     head     tail 
- *
- *   [ 1, 2 ]  => [ 2, 1 ] 
- *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]   
- * 
- */
-function swapHeadAndTail(arr) {
-    throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+    let h = new Date(date).getUTCHours();
+    h = h % 12;
+    let min = new Date(date).getUTCMinutes();
+    let angle = Math.abs(1/2 * (60 * h - 11 * min));
+    if (angle > 180) {
+        angle = (360 - angle);
+    }
+    return angle / 180 * Math.PI;
 }
 
 
 module.exports = {
-    findElement: findElement,
-    generateOdds: generateOdds,
-    doubleArray: doubleArray,
-    getArrayOfPositives: getArrayOfPositives,
-    getArrayOfStrings: getArrayOfStrings,
-    removeFalsyValues: removeFalsyValues,
-    getUpperCaseStrings: getUpperCaseStrings,
-    getStringsLength: getStringsLength,
-    insertItem: insertItem,
-    getHead: getHead,
-    getTail: getTail,
-    toCsvText: toCsvText,
-    toStringList: toStringList,
-    toArrayOfSquares: toArrayOfSquares,
-    getMovingSum: getMovingSum,
-    getSecondItems: getSecondItems,
-    propagateItemsByPositionIndex: propagateItemsByPositionIndex,
-    get3TopItems: get3TopItems,
-    getPositivesCount: getPositivesCount,
-    sortDigitNamesByNumericOrder: sortDigitNamesByNumericOrder,
-    getItemsSum: getItemsSum,
-    getFalsyValuesCount: getFalsyValuesCount,
-    findAllOccurences: findAllOccurences,
-    sortCitiesArray: sortCitiesArray,
-    getIdentityMatrix: getIdentityMatrix,
-    getIntervalArray: getIntervalArray,
-    distinct: distinct,
-    group: group,
-    selectMany: selectMany,
-    getElementByIndexes: getElementByIndexes,
-    swapHeadAndTail: swapHeadAndTail
+    parseDataFromRfc2822: parseDataFromRfc2822,
+    parseDataFromIso8601: parseDataFromIso8601,
+    isLeapYear: isLeapYear,
+    timeSpanToString: timeSpanToString,
+    angleBetweenClockHands: angleBetweenClockHands
 };
